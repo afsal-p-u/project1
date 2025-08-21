bee9e960aa914a7380a834377d1e4c97347cb4a7
Vismaya <vismayava6@gmail.com>
2025-08-21 16:47:20 +0530

    update 21/08/25final

--- config/settings.py
+++ config/settings.py
@@ -39,8 +39,18 @@ SIMPLE_JWT = {
 
 # Application definition
 
-
-
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    "corsheaders",
+    'syncdata',
+]
 
 
 CORS_ALLOWED_ORIGINS = [
